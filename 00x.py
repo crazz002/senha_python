f48e8040f7dcfacad0693f078ead9c26367fb9fb
crazz002 <isaquevitor99@gmail.com>
2025-07-24 21:15:50 -0300

    Add files via upload

---
+++ 00x.py
@@ -0,0 +1,19 @@
+'''
+nome = input("Qual e o seu nome? ")
+print("Prazer em te conhecer",nome)
+
+
+senha = input("Defina uma senha")
+print("senha definida, digita suua senha")
+
+'''
+
+senha = input('Defina sua senha: ')
+print('Senha definida! Agora digite sua senha')
+
+senha_digitada = input('Digite sua senha: ')
+if senha_digitada == senha:
+    print('Senha correta!')
+else:
+    print('SENHA ERRADA!')
+
